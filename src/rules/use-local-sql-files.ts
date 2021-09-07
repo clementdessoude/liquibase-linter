9562d4c1dbd3241574bd18e4207dfe13c7ba2de7
lechinoix <nicolas.ngomai@gmail.com>
2021-09-07 14:58:20 +0200

    fix: fix code style

--- src/rules/use-local-sql-files.ts
+++ src/rules/use-local-sql-files.ts
@@ -4,9 +4,9 @@ import { Reporter } from "reporter";
 export const check = (changeSet: ChangeSet, reporter: Reporter) => {
   const hasRemoteSqlFilePath = changeSet.changes.some(change => {
     const sqlFilePath = change?.sqlFile?.path;
-    return sqlFilePath && !sqlFilePath.startsWith('./')
-  })
+    return sqlFilePath && !sqlFilePath.startsWith('./');
+  });
   if (hasRemoteSqlFilePath) {
-    reporter.error(`The changeSet contains an sql script imported from another directory. More details [here](https://github.com/clementdessoude/liquibase-linter/tree/main/src/rules/use-local-sql-files.md)`, changeSet.id);
+    reporter.error(`The changeSet contains a sql script imported from another directory. More details [here](https://github.com/clementdessoude/liquibase-linter/tree/main/src/rules/use-local-sql-files.md)`, changeSet.id);
   }
 };
